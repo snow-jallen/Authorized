6f951c1e4173517813271ac5ca58e9aebeea1e8f
Jonathan Allen <jallen@snow.edu>
2022-03-18 07:40:22 -0600

    Get UWP working against keycloak.

--- Authorized/Authorized.UWP/App.xaml.cs
+++ Authorized/Authorized.UWP/App.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using xf = Xamarin.Forms;
 
 namespace Authorized.UWP
 {
@@ -58,6 +49,8 @@ namespace Authorized.UWP
                 rootFrame.NavigationFailed += OnNavigationFailed;
                 global::Xamarin.Forms.Forms.SetFlags("Shell_UWP_Experimental");
                 Xamarin.Forms.Forms.Init(e);
+
+                xf.DependencyService.Register<WabBrowser>();
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
--- Authorized/Authorized/ViewModels/AboutViewModel.cs
+++ Authorized/Authorized/ViewModels/AboutViewModel.cs
@@ -1,18 +1,110 @@
-﻿using System;
-using System.Windows.Input;
-using Xamarin.Essentials;
+﻿using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Authorized.ViewModels
 {
-    public class AboutViewModel : BaseViewModel
+    public partial class AboutViewModel : ObservableObject
     {
         public AboutViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            Title = "About Time!";
+
+            var browser = DependencyService.Get<IBrowser>();
+            var options = new OidcClientOptions
+            {
+                Authority = "https://engineering.snow.edu/aspen/auth/realms/aspen",
+                ClientId = "aspen-web",
+                Scope = "profile email api-use",
+                RedirectUri = "xamarinformsclients://callback",
+                Browser = browser
+            };
+
+            _client = new OidcClient(options);
+            _apiClient.Value.BaseAddress = new Uri("https://engineering.snow.edu/aspen/");
+
+            OutputText = "Ready to go!";
         }
 
-        public ICommand OpenWebCommand { get; }
+
+        OidcClient _client;
+        LoginResult _result;
+        Lazy<HttpClient> _apiClient = new Lazy<HttpClient>(() => new HttpClient());
+        string accessToken;
+
+        [ObservableProperty]
+        private string title;
+
+        [ObservableProperty]
+        private string outputText;
+
+        [ICommand]
+        private async Task CopyJwt()
+        {
+            await Xamarin.Essentials.Clipboard.SetTextAsync(accessToken);
+        }
+
+        [ICommand]
+        private async Task Login()
+        {
+            try
+            {
+                _result = await _client.LoginAsync(new LoginRequest());
+
+                if (_result.IsError)
+                {
+                    OutputText = _result.Error;
+                    return;
+                }
+
+                var sb = new StringBuilder(128);
+                foreach (var claim in _result.User.Claims)
+                {
+                    sb.AppendFormat("{0}: {1}\n", claim.Type, claim.Value);
+                }
+
+                sb.AppendFormat("\n{0}: {1}\n", "refresh token", _result?.RefreshToken ?? "none");
+                sb.AppendFormat("\n{0}: {1}\n", "access token", _result.AccessToken);
+                accessToken = _result.AccessToken;
+
+                OutputText = sb.ToString();
+
+                _apiClient.Value.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _result?.AccessToken ?? "");
+            }
+            catch (Exception ex)
+            {
+                OutputText = ex.ToString();
+            }
+        }
+
+        [ICommand]
+        private async Task CallApi()
+        {
+            try
+            {
+                var result = await _apiClient.Value.GetAsync("api/admin");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    OutputText = JsonDocument.Parse(await result.Content.ReadAsStringAsync()).RootElement.GetRawText();
+                }
+                else
+                {
+                    OutputText = result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                OutputText = ex.ToString();
+            }
+        }
     }
 }
